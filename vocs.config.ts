8a8a8bb41cb642d22c290f55cd042aae2e7c4cc1
tmm <tmm@tmm.dev>
2026-04-06 13:19:09 -0400

    docs: html (#529)

--- vocs.config.ts
+++ vocs.config.ts
@@ -750,6 +750,19 @@ export default defineConfig({
                   },
                 ],
               },
+              {
+                text: "HTML reference",
+                items: [
+                  {
+                    text: "Html.init",
+                    link: "/sdk/typescript/Html.init",
+                  },
+                  {
+                    text: "Custom",
+                    link: "/sdk/typescript/html/custom",
+                  },
+                ],
+              },
               { text: "CLI reference", link: "/sdk/typescript/cli" },
             ],
           },
