d4e745008027db61ff29ef5f690b4916055ccfc8
Brendan Ryan <1572504+brendanjryan@users.noreply.github.com>
2026-07-24 15:36:04 -0700

    chore: remove public design system page

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -21,7 +21,6 @@ type Page =
   | { path: '/blog/sessions-improved'; render: 'static' }
   | { path: '/blog/subscriptions'; render: 'static' }
   | { path: '/brand'; render: 'static' }
-  | { path: '/design-system'; render: 'static' }
   | { path: '/extensions'; render: 'static' }
   | { path: '/faq'; render: 'static' }
   | { path: '/governance'; render: 'static' }
